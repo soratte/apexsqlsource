0890fb58392fcedf4c35371d5305b829e634e2ad
soratte <soratte>
2021-07-22 06:15:50 -0700

    add table_1

---
+++ Mio/Tables/dbo.Table_1.sql
@@ -0,0 +1,11 @@
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[Table_1] (
+		[col01]     [real] NULL,
+		[col02]     [varbinary](50) NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Table_1] SET (LOCK_ESCALATION = TABLE)
+GO
